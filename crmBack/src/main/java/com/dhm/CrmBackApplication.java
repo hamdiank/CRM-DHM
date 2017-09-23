11ef69ce3c09a6f76492bf13832c370caa45c91d
hamdiank <hamdi.ankoud@gmail.com>
2017-09-23 12:46:14 +0100

    create project

---
+++ crmBack/src/main/java/com/dhm/CrmBackApplication.java
@@ -0,0 +1,12 @@
+package com.dhm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmBackApplication.class, args);
+	}
+}
