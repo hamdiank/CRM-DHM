272a55110d6d9636d11dc68be58e3a5a024cc29f
anoirgu <guesmianoir@gmail.com>
2017-09-25 15:58:19 +0100

    adding the package .json

--- crmFront/src/app/app.module.ts
+++ crmFront/src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import {LoginComponent} from "./login/login.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {HeaderComponent} from "./shared/header/header.component";
+import {FooterComponent} from "./shared/footer/footer.component";
+import {SidebarComponent} from "./shared/sidebar/sidebar.component";
+import {LocationStrategy, HashLocationStrategy} from "@angular/common";
+import {RouterModule} from "@angular/router";
+import {AppRoute} from "./app-route";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MDBBootstrapModule.forRoot(),
+    RouterModule.forRoot(AppRoute)
   ],
-  providers: [],
+  schemas: [ NO_ERRORS_SCHEMA ],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
